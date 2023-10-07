497963e80e7bf795dd9fb775a6523c3b0e483507
rahilb1 <budhwanirahil@gmail.com>
2023-10-07 16:48:42 +0530

    Added JWT create and verify logic

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,18 +1,37 @@
-const userRepository = require('../repository/user-repository');
+const jwt = require("jsonwebtoken");
 
+const userRepository = require("../repository/user-repository");
+const { JWT_KEY } = require("../config/serverConfig");
 class UserService {
-    constructor() {
-        this.userRepository = new userRepository();
+  constructor() {
+    this.userRepository = new userRepository();
+  }
+  async create(data) {
+    try {
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the service layer");
+      throw error;
     }
-    async create(data) {
-        try {
-            const user = await this.userRepository.create(data);
-            return user;
-            
-        } catch (error) {
-            console.log("Something went wrong in the service layer");
-            throw error;
-        }
+  }
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+      throw error;
     }
+  }
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation", error);
+      throw error;
+    }
+  }
 }
 module.exports = UserService;
